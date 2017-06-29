ba396530dd909cf8b2458f9d46ff4ff2495fd1cf
Alan Hong <xiupan@gmail.com>
2017-06-29 14:47:31 -0500

    Setup the DB Validations. Not sure they will work though...

--- models/gabs.js
+++ models/gabs.js
@@ -1,10 +1,25 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var Gabs = sequelize.define('Gabs', {
-    user: DataTypes.STRING,
-    text: DataTypes.STRING,
+    user: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: 'Please enter a username.'
+        }
+      }
+    },
+    text: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: 'Please enter a gab.'
+        },
+        max: 140
+      }
+    },
     publishedAt: DataTypes.DATE,
-    likes: DataTypes.INTEGER
+    likes: DataTypes.ARRAY(DataTypes.STRING) // this may not work... but let's try it. Hoping to have an array of users who liked the gab.
   }, {
     classMethods: {
       associate: function(models) {
